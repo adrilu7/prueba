e44a2c33afa91c4c886619d0efec13dc7695c9fa
luz <adrivioleta@hotmail.com>
2018-06-02 22:46:21 -0500

    mi primer commit

---
+++ app.js
@@ -0,0 +1,6 @@
+//preguntar tu temperatura en celsius
+var temperature = parseInt(prompt ("Cual es la temperatura en celsius"));
+//convertir celsius a fahrenheit
+var convertToFahrenheit = (temperature*1.8) + 32;
+//Mostrar resultado en la consola
+console.log("La temperatura en fahrenheit es " + convertToFahrenheit);
